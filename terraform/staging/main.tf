6a9bf6b41442d51b7915c4bc93238b07dc40c205
elcorbs <emma@madetech.com>
2020-11-16 09:57:20 +0000

    Add ES staging security group to serverless config Pass region and account ID in staging elasticsearch setup Add terraform for a production elasticsearch domain.

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -92,4 +92,6 @@ module "elasticsearch_db_staging" {
     instance_type= "t2.small.elasticsearch"
     ebs_enabled= "true"
     ebs_volume_size= "10"
+    region= data.aws_region.current.name
+    account_id= data.aws_caller_identity.current.account_id
 }
