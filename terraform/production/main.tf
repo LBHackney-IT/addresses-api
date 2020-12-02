d36f4e7160885be1bd82a680d0dea7048ef8e3cc
Renny Fadoju <rennyvbirds@yahoo.com>
2020-12-02 18:59:03 +0000

    Move alarms into separate file and edit threshold

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -78,46 +78,6 @@ module "postgres_db_production" {
   project_name         = "platform apis"
 }
 
-/*    CLOUDWATCH ALARM SET UP    */
-
-data "aws_sns_topic" "platform_apis" {
-  name = "Platform-APIs-Alerts"
-}
-
-resource "aws_cloudwatch_metric_alarm" "database-transactions-log-disk-usage" {
-  alarm_name          = "database-transactions-log-disk-usage"
-  alarm_description   = "This metric monitors transactions log disk usage goes above a threshold"
-  alarm_actions       = ["${data.aws_sns_topic.platform_apis.arn}"]
-  comparison_operator = "GreaterThanThreshold"
-  threshold           = "3000"
-  evaluation_periods  = "1"
-  metric_name         = "TransactionLogsDiskUsage"
-  namespace           = "RDS"
-  period              = "300"
-  statistic           = "Average"
-
-  dimensions = {
-    DBInstanceIdentifier = "${module.postgres_db_production.instance_id}"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "free_storage_space_too_low" {
-  alarm_name          = "free-storage-space-too-low"
-  alarm_description   = "Average database free storage space too low within the last 5 minutes"
-  alarm_actions       = ["${data.aws_sns_topic.platform_apis.arn}"]
-  comparison_operator = "LessThanThreshold"
-  threshold           = "40000"
-  evaluation_periods  = "1"
-  metric_name         = "FreeStorageSpace"
-  namespace           = "RDS"
-  period              = "300"
-  statistic           = "Average"
-
-  dimensions = {
-    DBInstanceIdentifier = "${module.postgres_db_production.instance_id}"
-  }
-}
-
 /*    ELASTICSEARCH SETUP    */
 
 module "elasticsearch_db_production" {
