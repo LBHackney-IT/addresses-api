67ff4df9652fb7bb30bf93315cf6501980eb3112
AtiyyahRawat <atiyyah.rawat@gmail.com>
2025-01-16 14:00:00 +0000

    Upgrade .NET 6.0 => 8.0

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 dotnet restore
-dotnet lambda package --project-location ./Reindex --configuration release --framework net6.0 --output-package ./bin/release/net6.0/reindex-es-alias.zip
-dotnet lambda package --project-location ./AddressesAPI --configuration release --framework net6.0 --output-package ./bin/release/net6.0/addresses-api.zip
+dotnet lambda package --project-location ./Reindex --configuration release --framework net8.0 --output-package ./bin/release/net8.0/reindex-es-alias.zip
+dotnet lambda package --project-location ./AddressesAPI --configuration release --framework net8.0 --output-package ./bin/release/net8.0/addresses-api.zip
