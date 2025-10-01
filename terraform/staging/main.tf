016cb8a7102dc82a97249d492b847d484d6109c0
Miles Alford <miles.alford@hackney.gov.uk>
2025-10-01 12:01:16 +0100

    Create dms vpc role to allow creation of replication instance

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -121,6 +121,16 @@ data "aws_iam_policy_document" "dms-assume-role-policy" {
   }
 }
 
+resource "aws_iam_role" "dms-vpc-role" {
+  assume_role_policy = "${data.aws_iam_policy_document.dms-assume-role-policy.json}"
+  name               = "dms-vpc-role"
+}
+
+resource "aws_iam_role_policy_attachment" "dms-vpc-role-AmazonDMSVPCManagementRole" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSVPCManagementRole"
+  role       = "${aws_iam_role.dms-vpc-role.name}"
+}
+
 resource "aws_iam_role" "dms_service_role" {
   name               = "dms_service_role"
   path               = "/system/"
