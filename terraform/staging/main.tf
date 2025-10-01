74f36051ceeac47e90d53e0026a11c123532cd28
Miles Alford <miles.alford@hackney.gov.uk>
2025-10-01 13:39:47 +0100

    Remove string interpolation for IAM role attributes

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -122,13 +122,13 @@ data "aws_iam_policy_document" "dms-assume-role-policy" {
 }
 
 resource "aws_iam_role" "dms-vpc-role" {
-  assume_role_policy = "${data.aws_iam_policy_document.dms-assume-role-policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.dms-assume-role-policy.json
   name               = "dms-vpc-role"
 }
 
 resource "aws_iam_role_policy_attachment" "dms-vpc-role-AmazonDMSVPCManagementRole" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonDMSVPCManagementRole"
-  role       = "${aws_iam_role.dms-vpc-role.name}"
+  role       = aws_iam_role.dms-vpc-role.name
 }
 
 resource "aws_iam_role" "dms_service_role" {
