51817525cef4b7182cf2a21dc127f7583d083938
duslerke <43747286+Duslerke@users.noreply.github.com>
2026-06-26 07:20:50 +0100

    Resolve staging postgres database terraform drift.

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -68,7 +68,7 @@ module "postgres_db_staging" {
   db_port               = 5502
   subnet_ids            = data.aws_subnet_ids.staging.ids
   db_engine             = "postgres"
-  db_engine_version     = "16.8"
+  db_engine_version     = "16.13"
   db_instance_class     = "db.t3.micro"
   db_allocated_storage  = 100
   maintenance_window    = "sun:10:00-sun:10:30"
