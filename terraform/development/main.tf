dc1cf0f57c41a597ec2a75662d715ddc31e5a0e7
LBHTKarki <tuomo.karki@hackney.gov.uk>
2026-07-23 10:09:11 +0100

    bump up es resources

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -164,8 +164,8 @@ module "elasticsearch_db_development" {
   project_name     = "addresses-api"
   es_version       = "7.10"
   encrypt_at_rest  = "true"
-  instance_type    = "t3.small.elasticsearch"
-  instance_count   = "1"
+  instance_type    = "t3.medium.elasticsearch"
+  instance_count   = "2"
   ebs_enabled      = "true"
   ebs_volume_size  = "30"
   region           = data.aws_region.current.name
