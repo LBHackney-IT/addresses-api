5f8b291eb37059cf5fa076a8c62cb8a55b77f77a
LBHTKarki <tuomo.karki@hackney.gov.uk>
2026-07-24 09:40:46 +0100

    increase es space to 50

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -201,7 +201,7 @@ module "elasticsearch_db_development" {
   instance_type    = "t3.medium.elasticsearch"
   instance_count   = "3"
   ebs_enabled      = "true"
-  ebs_volume_size  = "40"
+  ebs_volume_size  = "50"
   region           = data.aws_region.current.name
   account_id       = data.aws_caller_identity.current.account_id
 
